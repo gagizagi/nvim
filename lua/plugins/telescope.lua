c716580b6ab22cce54424a46612b1a973a266951
Gasper Zagar <gasper.zagar@iprom.si>
2024-02-25 20:54:56 +0100

    move telescope keys to plugin config and add telescope file_browser

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,4 +1,13 @@
 return {
-	'nvim-telescope/telescope.nvim', tag = '0.1.5',
-	dependencies = { 'nvim-lua/plenary.nvim' }
+	'nvim-telescope/telescope-file-browser.nvim',
+	dependencies = { 'nvim-telescope/telescope.nvim', 'nvim-lua/plenary.nvim' },
+	config = function()
+		local telescope = require("telescope")
+		telescope.load_extension("file_browser")
+	end,
+	keys = {
+		{ '<leader>ff', '<cmd>Telescope file_browser<CR>', desc = '[F]ind [F]iles' },
+		{ '<leader><space>', '<cmd>Telescope buffers<CR>', desc = '[F]ind [B]uffers' },
+		{ '<leader>fg', '<cmd>Telescope live_grep<CR>', desc = '[F]ind [G]rep' },
+	},
 }
